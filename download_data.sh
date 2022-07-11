8635999c99e163d42e46ff3150425d6845254afb
Karmim Yannis <karmim@pop-os.localdomain>
2022-07-11 17:19:09 +0200

    datasets

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+
+wget http://snap.stanford.edu/jodie/reddit.csv -P data/
+wget http://snap.stanford.edu/jodie/wikipedia.csv -P data/
+wget http://snap.stanford.edu/jodie/mooc.csv -P data/
+wget http://snap.stanford.edu/jodie/lastfm.csv -P data/
